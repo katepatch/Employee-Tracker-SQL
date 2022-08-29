55d8cf4095c78a1444cea0cb81cfff03020c12b8
kate patch <kate.epatch@gmail.com>
2022-08-29 17:17:48 -0600

    created role values

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,16 @@
 INSERT INTO department (name)
-VALUES ('Sales'), ('Engineering'), ('Finance'), ('Legal')
+VALUES ('Sales'), ('Engineering'), ('Finance'), ('Legal');
+
+INSERT INTO role(title, salary, department_id)
+VALUES ('Sales Rep', 65000, 1),
+        ('Sales Manager', 90000, 1),
+        ('Sales Executive', 115000, 1),
+        ('Design Engineer', 135000, 2),
+        ('Industrial Engineer', 200000, 2),
+        ('Chemical Engineer', 175000, 2),
+        ('Accountant', 85000, 3),
+        ('Accounting Manager', 125000, 3),
+        ('Controller', 155000, 3),
+        ('Legal Assistant', 75000, 4),
+        ('Lawyer', 275000, 4);
+
