1aa418f3ccf88f964dc9c90544b35093a70b0988
kate patch <kate.epatch@gmail.com>
2022-08-29 18:06:59 -0600

    added mysql createconnection function

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'employee_db'
+    },
+    console.log('Connected to employee_db database')
+);
