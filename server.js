f3fdb5fa19c1c26dff0500681be71c9132b027cc
kate patch <kate.epatch@gmail.com>
2022-08-29 18:31:25 -0600

    added prompt to chose what you want to view

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 const Connection = require('mysql2/typings/mysql/lib/Connection');
+const { allowedNodeEnvironmentFlags } = require('process');
 
 const PORT = process.env.PORT || 3001;
 
@@ -33,3 +34,53 @@ afterConnection = () => {
     ───────╚╝──────╚══╝─────────────────────╚══╝`)
     infoPrompt();
 };
+
+const infoPrompt = () => {
+    inquirer.prompt ([
+        {
+            type: 'list', 
+            name: 'choices',
+            message: 'What would you like to do?',
+            choices: [
+                'View All Departments',
+                'View All Roles',
+                'View All Employees',
+                'Add a Department',
+                'Add a Role',
+                'Add an Employee',
+                'Update Employee Role'
+            ]
+        }
+    ])
+    .then(answers) => {
+        const { choices } = answers;
+
+        if (choices === "View All Departments") {
+            showDepartments();
+        }
+
+        if (choices === "View All Roles") {
+            showRoles();
+        }
+
+        if (choices === "View All Employees") {
+            showEmployees();
+        }
+
+        if (choices === "Add a Department") {
+            addDepartment();
+        }
+
+        if (choices === "Add a Role") {
+            addRole();
+        }
+
+        if (choices === "Add an Employee") {
+            addEmployee();
+        }
+
+        if (choices === "Update Employee Role") {
+            updateEmployee();
+        }
+    }
+}
