d3e1b4170acae9b23a51d035ee6916d73b01703d
kate patch <kate.epatch@gmail.com>
2022-08-30 17:41:59 -0600

    finished updateEmployee function  now i have to figure out the promise

--- server.js
+++ server.js
@@ -299,3 +299,60 @@ addEmployee = () => {
     });
 };
 
+updateEmployee = () => {
+    const employeeSql = `SELECT * FROM employee`;
+
+    connection.promise().query(employeeSql, (err, data) => {
+        if (err) throw err;
+
+        const employees = data.map (({ id, first_name, last_name })=> ({ name: first_name + " "+ last_name, value: id }));
+
+        inquirer.prompt ([
+            {
+                type: 'list',
+                name: 'name',
+                message: 'Which employee do you want to update?',
+                choices: employees
+            }
+        ])
+        .then(employeeChoice => {
+            const employee = employeeChoice.name;
+            const params = [];
+            params.push(employee);
+
+            const roleSql = `SELECT * FROM role`;
+
+            connection.promise().query(roleSql, (err, data) => {
+                if (err) throw err;
+
+                const roles = data.map(({ id, title }) => ({ name: title, value: id }));
+
+                inquirer.prompt([
+                    {
+                        type: 'list',
+                        name: 'role',
+                        message: "What is the employee's new role?",
+                        choices: roles
+                    }
+                ])
+                .then(roleChoice => {
+                    const role = roleChoice.role;
+                    params.push(role);
+
+                    let employee = params[0]
+                    params[0] = role
+                    params[1] = employee
+
+                    const sql = `UPDATE employee SET role_id = ? WHERE id = ?`;
+
+                    connection.query(sql, params, (err, result) => {
+                        if (err) throw err;
+                        console.log('You have successfully updated employee');
+
+                        showEmployees();
+                    });
+                });
+            });
+        });
+    });
+};
