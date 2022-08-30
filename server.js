292a60d8909d670d34016a9ad7c249715ac59620
kate patch <kate.epatch@gmail.com>
2022-08-29 18:07:44 -0600

    added  port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 
+const PORT = process.env.PORT || 3001;
+
 const db = mysql.createConnection(
     {
         host: 'localhost',
